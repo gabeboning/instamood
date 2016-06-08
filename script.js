ea9936bfe8d0827d1f080be8aab378e693d4d862
Rafi Khan <rafi.fa.khan@gmail.com>
2016-06-08 04:21:53 +0200

    Stage 2

--- script.js
+++ script.js
@@ -4,25 +4,32 @@ $(document).ready(function() {
     window.location.replace("./login.html");
   }
   token = token.replace("#", "?"); // Ready for query param.
-  var API_BASE_URL = "https://api.instagram.com/v1";
+  getInstaPics(token);
+});
+
+function getInstaPics(token) {
+  var INSTA_API_BASE_URL = "https://api.instagram.com/v1";
   var path = "/users/self/media/recent";
-  var mediaUrl = API_BASE_URL + path + token;
+  var mediaUrl = INSTA_API_BASE_URL + path + token;
   $.ajax({
+    method: "GET",
     url: mediaUrl,
     dataType: "jsonp",
     success: function(response) {
       showPictures(response.data);
+      analyzeSentiments(response.data);
     }
   });
-});
+}
 
 function showPictures(data) {
-  console.log(data);
   $("#list").html("");
   for (var i=0; i<data.length; i++) {
     var picData = data[i];
     var postDiv = $("<div></div>");
     postDiv.addClass("post");
+    // Mark this as this with this ID so we can find it for sentiment analysis.
+    postDiv.attr("id", "post-" + i); 
     var img = $("<img />")
     img.attr("src", picData.images.standard_resolution.url);
     var caption = $("<p></p>");
@@ -32,3 +39,76 @@ function showPictures(data) {
     $("#list").append(postDiv);
   }
 }
+
+function analyzeSentiments(data) {
+  $.each(data, function(index, value) {
+    var phrase = value.caption.text;
+    var SENTIMENT_API_BASE_URL =
+      "https://twinword-sentiment-analysis.p.mashape.com/analyze/";
+    $.ajax({
+      method: "POST",
+      url: SENTIMENT_API_BASE_URL,
+      headers: {
+        "X-Mashape-Key": "H0IoG8ybyymshvm2IlQmwZk8Vlqlp1KD3F2jsn2RLpghqszsGI"
+      },
+      data: {text: phrase},
+      success: function(response) {
+        console.log(response);
+        addSentiment(response.type, response.score, index);
+      }
+    });
+  });
+}
+
+function addSentiment(type, score, picNum) {
+  console.log(allSentimentScores, score);
+  var picDiv = $("#post-" + picNum);
+  var sentimentDiv = $("<div></div>");
+  var sentimentI = $("<i></i>");
+  sentimentI.addClass("fa");
+  var faClass = "fa-meh-o";
+  if (type === "positive") {
+    sentimentDiv.addClass("positive");
+    faClass = "fa-smile-o";
+  } else if (type === "negative") {
+    sentimentDiv.addClass("negative");
+    faClass = "fa-frown-o";
+  }
+  sentimentI.addClass(faClass);
+
+  sentimentDiv.append("Sentiment: ").append(sentimentI)
+    .append(" (score: " + score.toFixed(2) + ")");
+  picDiv.append(sentimentDiv);
+  
+  updateTotalSentiment(score);
+}
+
+var allSentimentScores = [];
+function updateTotalSentiment(score) {
+  allSentimentScores.push(score);
+  console.log(allSentimentScores, score);
+  var sum = 0;
+  for (var i=0; i<allSentimentScores.length; i++) {
+    sum += allSentimentScores[i];
+  }
+  var avg = sum / allSentimentScores.length;
+  var text = "Neutral"
+  var textClass = "";
+  if (avg > 0) {
+    text = "Positive!";
+    textClass = "positive";
+  } else if (avg < 0) {
+    text = "Negative :(";
+    textClass = "negative";
+  }
+
+  $("#mood").html(text + " (score: " + avg + ")");
+  $("#mood").addClass(textClass);
+}
+
+
+
+
+
+
+
